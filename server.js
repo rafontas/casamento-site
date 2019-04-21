45e98973b3141a8e9cbfdf3c4dbc56c0b002b2b0
Rafael Freitas <rafontas@gmail.com>
2019-04-21 12:28:18 -0300

    Ajustando configuracao servidor

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ var server_port = process.env.PORT || 3000;
 var server_host = '0.0.0.0';
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/casamento-site'));
+// app.use(express.static(__dirname + '/dist/casamento-site'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/casamento-site/index.html'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/casamento-site/index.html'));
+// });
+
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
-// default Heroku port
-app.listen(server_port, server_host, function(){
-    console.log('Erro ao iniciar');
-});
-    
-//  server.listen(server_port, function() {
-//      console.log('Listening on port %d', server_port);
-//  });
-//  
-// server.listen(server_port);
+
+// Teste 1
+// app.listen(server_port, server_host, function(){
+//     console.log('Erro ao iniciar');
+// });

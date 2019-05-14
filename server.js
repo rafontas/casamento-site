ecbabfdc68a669aa1f8d76fc256fff73425d2361
Rafael Freitas <rafontas@gmail.com>
2019-05-13 21:38:44 -0300

    Ajustando os parâmetros do Heroku.

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var server_port = process.env.PORT || 3000;
 var server_host = '0.0.0.0';
 
 
-server.listen(server_port, server_host, function() {
+var server = app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });

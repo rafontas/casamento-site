53da0943f2d5d05b1e7694ebeba69902ea039911
Rafael Freitas <rafontas@gmail.com>
2019-05-17 10:34:41 -0300

    Ajustando configuração Heroku

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-var server_port = process.env.PORT || 3000;
+var server_port = process.env.PORT || 3001;
 var server_host = '0.0.0.0';
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+// app.use(express.static('dist'));
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);

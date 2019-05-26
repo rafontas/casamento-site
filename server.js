41519ce0fa13e260bf0e12310c0294bd8eaa4dba
Rafael Freitas <rafontas@gmail.com>
2019-05-25 23:34:39 -0300

    Alterando o caminho das imagens pequenas para o deploy.

--- server.js
+++ server.js
@@ -3,17 +3,8 @@ const express = require('express');
 const app = express();
 var server_port = process.env.PORT || 3001;
 var server_host = '0.0.0.0';
-/*
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-*/
 
 app.use(express.static('dist/casamento-site'));
-/*app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '/dist/casamento-site', 'index.html'));
-});
-*/
 
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);

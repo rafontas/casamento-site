1acffdd386e1fec0a5b8ce1f3d140c4ce8a1cc44
Rafael Freitas <rafontas@gmail.com>
2019-05-26 00:26:01 -0300

    Alterando o heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 var server_port = process.env.PORT || 3001;
 var server_host = '0.0.0.0';
 
-app.use(express.static('dist/casamento-site'));
+// app.use(express.static('dist/casamento-site'));
+app.use(express.static('dist'));
 
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);

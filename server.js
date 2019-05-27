e9c7065a16aff7e7ca7ced03aa367ae6ab413af1
Rafael Freitas <rafontas@gmail.com>
2019-05-26 21:05:56 -0300

    Ajustando as fotos

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// var server_port = process.env.PORT || 3001;
-// var server_host = '0.0.0.0';
-
-// app.use(express.static('dist/casamento-site'));
-// app.use(express.static('src/app/images/'));
-// // app.use(express.static('dist'));
-
-// app.listen(server_port, server_host, function() {
-//     console.log('Listening on port %d', server_port);
-// });
-
 var server_port = process.env.PORT || 3000;
 var server_host = '0.0.0.0';
 
